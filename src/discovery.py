21494e4c121cd1011d8207e9aa4f05421da824b9
Sung Gon Yi <skonmeme@gmail.com>
2019-01-02 10:31:12 +0900

    get log URLs

--- src/discovery.py
+++ src/discovery.py
@@ -13,7 +13,17 @@ from functools import partial
 
 logger = logging.getLogger("discovery")
 
-def flink_cluster_overview(jm_url):
+
+def get_yarn_application_info(app_id, rm_addr):
+    r = requests.get(rm_addr + '/ws/v1/cluster/apps/' + app_id)
+    if r.status_code != 200:
+        return {}
+
+    decoded = r.json()
+    return decoded['app'] if 'app' in decoded else {}
+
+
+def get_flink_cluster_overview(jm_url):
     r = requests.get(jm_url+'/overview')
     if r.status_code != 200:
         return {}
@@ -21,71 +31,7 @@ def flink_cluster_overview(jm_url):
     return decoded
 
 
-def flink_jobmanager_prometheus_addr(jm_url):
-    addr = None
-    port = None
-
-    r = requests.get(jm_url+'/jobmanager/config')
-    if r.status_code != 200:
-        return ''
-    dic = {}
-    for obj in r.json():
-        dic[obj['key']] = obj['value']
-    addr = dic['jobmanager.rpc.address']
-
-    r = requests.get(jm_url + '/jobmanager/log', stream=True)
-    if r.status_code != 200:
-        return ''
-    for line in r.iter_lines(decode_unicode=True):
-        if "Started PrometheusReporter HTTP server on port" in line:
-            m = re.search('on port (\d+)', line)
-            if m:
-                port = m.group(1)
-                break
-
-    cond1 = addr is not None
-    cond2 = port is not None
-    if cond1 and cond2:
-        return addr+':'+port
-    else:
-        return ''
-
-
-def flink_taskmanager_prometheus_addr(tm_id, jm_url, version):
-    addr = None
-    port = None
-
-    r = requests.get(jm_url+'/taskmanagers/'+tm_id+'/log', stream=True)
-    if r.status_code != 200:
-        return ''
-
-    for line in r.iter_lines(decode_unicode=True):
-        if "hostname/address" in line:
-            if version.startswith("1.4"):
-                m = re.search("address '([0-9A-Za-z-_]+)' \([\d.]+\)", line)
-                if m:
-                    hostname = m.group(1)
-                    addr = hostname
-            elif version.startswith("1.5") or version.startswith("1.6"):
-                m = re.search("TaskManager: ([0-9A-Za-z-_]+)", line)
-                if m:
-                    hostname = m.group(1)
-                    addr = hostname
-
-        if "Started PrometheusReporter HTTP server on port" in line:
-            m = re.search('on port (\d+)', line)
-            if m:
-                port = m.group(1)
-
-        cond1 = addr is not None
-        cond2 = port is not None
-        if cond1 and cond2:
-            return addr+':'+port
-
-    return ''
-
-
-def taskmanager_ids(jm_url):
+def get_taskmanager_ids(jm_url):
     r = requests.get(jm_url + '/taskmanagers')
     if r.status_code != 200:
         return []
@@ -97,62 +43,51 @@ def taskmanager_ids(jm_url):
     return [tm['id'] for tm in decoded['taskmanagers']]
 
 
-def get_yarn_application_info(app_id, rm_addr):
-    r = requests.get(rm_addr + '/ws/v1/cluster/apps/' + app_id)
-    if r.status_code != 200:
-        return {}
-
-    decoded = r.json()
-    return decoded['app'] if 'app' in decoded else {}
-
-
 def find_flink_log_addresses(app_id, rm_addr):
-    prom_addrs = []
+    count = 1
+    log_url = {'app_id': app_id, 'rm_addr': rm_addr}
+
     while True:
+        if count > 10:
+            return {}
+
         app_info = get_yarn_application_info(app_id, rm_addr)
         logger.debug("Application ID: {}\nApplication Info: {}".format(app_id, app_info))
         if 'trackingUrl' not in app_info:
+            logger.debug("Application ID: {}, #{}".format(app_id, count))
+            count += 1
             time.sleep(1)
             continue
 
         jm_url = app_info['trackingUrl']
         jm_url = jm_url[:-1] if jm_url.endswith('/') else jm_url
+        log_url['jm_url'] = jm_url
 
-        overview = flink_cluster_overview(jm_url)
+        overview = get_flink_cluster_overview(jm_url)
         version = overview['flink-version']
         taskmanagers = overview['taskmanagers']
         logger.debug("Flink overview: {}\n  Version: {}\n  Task managers: {}".format(overview, version, taskmanagers))
 
+        log_url = {jm_log_url: jm_url + "/jobmanager/log"}
+
         if app_info['runningContainers'] == 1:
-            print("runningContainers(%d) is 1" % (app_info['runningContainers'],))
+            logger.info("runningContainers(%d) is 1" % (app_info['runningContainers'],))
+            count += 1
             time.sleep(1)
             continue
 
         if app_info['runningContainers'] != taskmanagers + 1:
-            print("runningContainers(%d) != jobmanager(1)+taskmanagers(%d)" % (
-            app_info['runningContainers'], taskmanagers))
+            logger.info("runningContainers(%d) != jobmanager(1)+taskmanagers(%d)" % (app_info['runningContainers'], taskmanagers))
+            count += 1
             time.sleep(1)
             continue
 
-        tm_ids = taskmanager_ids(jm_url)
-        prom_addrs = map(partial(flink_taskmanager_prometheus_addr, jm_url=jm_url, version=version), tm_ids)
-        prom_addrs = list(filter(lambda x: len(x) > 0, prom_addrs))
-        if len(tm_ids) != len(prom_addrs):
-            print("Not all taskmanagers open prometheus endpoints. %d of %d opened" % (len(tm_ids), len(prom_addrs)))
-            time.sleep(1)
-            continue
+        tm_ids = get_taskmanager_ids(jm_url)
+        log_url['tm_log_urls'] = [ jm_url + "taskmanagers/" + tm_id + "/log" for tm_id in tm_ids ]
         break
+    logger.debug(log_url)
 
-    while True:
-        jm_prom_addr = flink_jobmanager_prometheus_addr(jm_url)
-        if len(jm_prom_addr) == 0:
-            time.sleep(1)
-            continue
-        prom_addrs.append(jm_prom_addr)
-        break
-
-    encoded = json.JSONEncoder().encode([{'targets': prom_addrs}])
-    return encoded
+    return log_url
 
 
 def generate_logstash_conf(logs):
@@ -189,7 +124,7 @@ def main():
         logger.setLevel(logging.INFO)
     if args.v:
         logger.addHandler(logging.StreamHandler(stream=sys.stderr))
-        
+
     app_id = args.app_id
     name_filter_regex = None if args.name_filter is None else re.compile(args.name_filter)
     rm_addr = args.rm_addr if "://" in args.rm_addr else "http://" + args.rm_addr
@@ -234,10 +169,10 @@ def main():
                 removed = set(running_prev.keys()) - set(running_cur.keys())
 
             if len(added) + len(removed) > 0:
-                logging.info('====', time.strftime("%c"), '====')
-                logging.info('# running apps : ', len(running_cur))
-                logging.info('# added        : ', added)
-                logging.info('# removed      : ', removed)
+                logger.info('==== {} ===='.format(time.strftime("%c")))
+                logger.info("{} running apps : ".format(len(running_cur)))
+                logger.info("{} added        : ".format(added))
+                logger.info("{} removed      : ".format(removed))
 
                 logs = { app_id: find_flink_log_addresses(app_id, rm_addr) for app_id in running_cur.keys() }
                 [ logs.pop(key, None) for (key, value) in logs.items() if value is None ]
